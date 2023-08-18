aff5196e74d1914643f164e624bb50a70786fbbb
nisakib07 <websakib07@gmail.com>
2023-08-18 16:38:53 +0600

    project done

--- scripts/area-calculate.js
+++ scripts/area-calculate.js
@@ -1,61 +1,83 @@
 function triangleArea() {
   const base = getInputValue("triangleBase");
-  if (isNaN(base)) {
-    alert("Input Numeric Value");
-    return;
-  }
   const height = getInputValue("triangleHeight");
-  if (isNaN(height)) {
+  if (isNaN(height) || isNaN(base)) {
     alert("Input Numeric Value");
     return;
   }
   const area = 0.5 * base * height;
-  setResultInnerText("rectangleResult", area);
+  setResultInnerText("triangleResult", area);
+
+  addToLog("Triangle", area);
 }
 function rectangleArea() {
   const width = getInputValue("rectangleWidth");
-  if (isNaN(width)) {
-    alert("Input Numeric Value");
-    return;
-  }
   const length = getInputValue("rectangleLength");
-  if (isNaN(length)) {
+  if (isNaN(length) || isNaN(width)) {
     alert("Input Numeric Value");
     return;
   }
 
   const area = width * length;
   setResultInnerText("rectangleResult", area);
+
+  addToLog("Rectangle", area);
 }
 
 function parallelogramArea() {
   const base = getInputValue("parallelogramBase");
-  if (isNaN(base)) {
-    alert("Input Numeric Value");
-    return;
-  }
+
   const height = getInputValue("parallelogramHeight");
-  if (isNaN(height)) {
+  if (isNaN(height) || isNaN(base)) {
     alert("Input Numeric Value");
     return;
   }
 
   const area = base * height;
-  setResultInnerText("rectangleResult", area);
+  setResultInnerText("parallelogramResult", area);
+
+  addToLog("Parallelogram", area);
+}
+
+function rhombusArea() {
+  const d1 = getInputValue("rhombusd1");
+  const d2 = getInputValue("rhombusd2");
+
+  if (isNaN(d1) || isNaN(d2)) {
+    alert("Input Numeric Value");
+    return;
+  }
+
+  const area = 0.5 * d1 * d2;
+  setResultInnerText("rhombusResult", area);
+
+  addToLog("Rhombus", area);
+}
+
+function pentagonArea() {
+  const perimeter = getInputValue("pentagonPerimeter");
+  const apothem = getInputValue("pentagonApothem");
+
+  if (isNaN(perimeter) || isNaN(apothem)) {
+    alert("Input Numeric Value");
+    return;
+  }
+
+  const area = 0.5 * perimeter * apothem;
+  setResultInnerText("pentagonResult", area);
+  addToLog("Pentagon", area);
 }
 
 function ellipseArea() {
   const firstRadius = getInputValue("ellipseFirstRadius");
-  if (isNaN(firstRadius)) {
-    alert("Input Numeric Value");
-    return;
-  }
   const secondRadius = getInputValue("ellipseSecondRadius");
-  if (isNaN(secondRadius)) {
+  if (isNaN(secondRadius) || isNaN(firstRadius)) {
     alert("Input Numeric Value");
     return;
   }
 
   const area = (Math.PI * firstRadius * secondRadius).toFixed(3);
   setResultInnerText("ellipseResult", area);
+
+  addToLog("Ellipse", area);
 }
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -12,3 +12,18 @@ function setResultInnerText(areaId, area) {
   const resultArea = document.getElementById(areaId);
   resultArea.innerText = area;
 }
+
+function addToLog(shape, area) {
+  console.log("adding soon");
+
+  const elementEntry = document.getElementById("elementEntry");
+
+  const count = elementEntry.childElementCount;
+  const p = document.createElement("p");
+  p.classList.add("my-6");
+  p.innerHTML = `${
+    count + 1
+  }. ${shape} ${area} cm<sup>2</sup> <button class="btn btn-primary">Convert</button>`;
+
+  elementEntry.appendChild(p);
+}
